3d2c3b988c438b3e357b141da663b08882b87394
Thomas Johnston <trjohnst211@gmail.com>
2018-11-07 01:45:14 -0800

    Adding more product operations

--- routes/product.route.js
+++ routes/product.route.js
@@ -4,8 +4,9 @@ const router = express.Router();
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const product_controller = require('../controllers/product.controller');
 
-
-// a simple test url to check that all of our files are communicating correctly.
-router.get('/test', product_controller.test);
+router.get('/', product_controller.products_list);
 router.post('/create', product_controller.product_create);
+router.get('/:id', product_controller.product_details);
+router.put('/:id/update', product_controller.product_update);
+router.delete('/:id/delete', product_controller.product_delete);
 module.exports = router;
