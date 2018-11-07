bfd5c681f73634b88e6884aa15ce9c7cd9b44f7f
Thomas Johnston <trjohnst211@gmail.com>
2018-11-07 12:24:48 -0800

    Cleaning up app.js

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
-//app.js
-const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const product = require('./routes/product.routes');
 const views = require('./routes/views.routes');
+
 const app = express();
 
 // Set up mongoose connection
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://root:fpEvTQWv5iYnZzha@127.0.0.1:27017/productstutorial?authSource=admin', {
   useNewUrlParser: true,
   connectTimeoutMS: 750,
@@ -16,13 +16,16 @@ mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+// Setup API
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/api/products', product);
 
+// Setup Views
 app.set('view engine', 'pug');
 app.use('/', views);
 
+// Launch App
 let port = 1234;
 
 app.listen(port, () => {
